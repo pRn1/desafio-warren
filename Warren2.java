139ca0778fbc5769a3828687233da504ec0fb79f
pRn1 <99836240+pRn1@users.noreply.github.com>
2022-05-02 23:13:57 -0300

    Add files via upload

---
+++ Warren2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Warren2 {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+
+        int x , tempoChegada, qtdAlunos, normal = 0, atraso = 0;
+        int[] chegada;
+   
+        System.out.println("Informe a quantidade de alunos da turma: ");
+        qtdAlunos = leitor.nextInt();
+        
+     
+        System.out.println("Informe o limite de alunos atrasados: ");
+        x = leitor.nextInt();
+        
+        chegada = new int[qtdAlunos];
+        
+        for(int i = 0; i < qtdAlunos; i++){
+            System.out.println("Informe o tempo de chegada do aluno: ");
+            tempoChegada = leitor.nextInt();
+            chegada[i] = tempoChegada;
+            if (tempoChegada <= 0 ){
+                normal++;
+            }else{
+                atraso++;
+            }
+        }
+        System.out.println("Limite alunos atrasados = " + x);
+        System.out.println("Tempos de chegada em relacao ao inicio da aula = " 
+                + Arrays.toString(chegada));
+        
+        if(atraso > x){
+            System.out.println("Aula cancelada.");
+        }else{
+            System.out.println("Aula normal.");
+        }
+   
+    }
+}
