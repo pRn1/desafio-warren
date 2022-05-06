a8492782199a8d77aa006d046a463acb664d6961
pRn1 <99836240+pRn1@users.noreply.github.com>
2022-05-05 22:44:04 -0300

    Add files via upload

---
+++ Warren1.java
@@ -0,0 +1,45 @@
+public class Warren1{
+  
+    public static int reverso(int n){
+    
+        int reverso = 0;
+        
+        while (n > 0){
+            reverso = 10 * reverso + n % 10;
+            n /= 10;
+        }
+    return reverso;
+}
+    public static boolean verificaImpares ( int soma){
+        boolean resposta = false;
+        
+        String numero = String.valueOf(soma); 
+        char[] digitos = numero.toCharArray();
+        
+        for(int i = 0; i < digitos.length; i++){
+            resposta = digitos[i] % 2 != 0;
+            if(resposta == false){
+                break;
+            }
+    }
+
+    return resposta;
+}
+    public static void main(String[] args) {
+        
+       	int n, tamanho = 0; 
+ 
+        for(n = 0; n < 1000000; n++){
+            int soma = n + reverso(n);
+                if( n % 10 != 0){
+                    if(soma % 2 != 0){ 
+                        if(verificaImpares(soma) == true){
+                        System.out.println(n);
+                        tamanho++; 
+                        }
+                    }
+                }
+            }
+        System.out.println(tamanho);  
+    }     
+  }
